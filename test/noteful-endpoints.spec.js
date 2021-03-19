303f022cec0bbecd1d99b0334af35ed04533ed74
Tatia burdett <tatiaburdett@gmail.com>
2021-03-19 15:07:09 -0700

    write delete endpoint test

--- test/noteful-endpoints.spec.js
+++ test/noteful-endpoints.spec.js
@@ -1,3 +1,4 @@
+const { expect } = require('chai')
 const knex = require('knex')
 const supertest = require('supertest')
 const app = require('../src/app')
@@ -86,4 +87,84 @@ describe('Noteful Endpoints', function() {
       })
     })
   })
+
+  describe('POST /api/folder', () => { // POST 
+    it('creates a folder, responding with 201 and the new folder', () => { // CREATES A FOLDER
+      const newFolder = {
+        folder_name: 'New Test Folder'
+      }
+      return supertest(app)
+        .post('/api/folder')
+        .send(newFolder)
+        .expect(201)
+        .expect(res => {
+          expect(res.body.folder_name).to.eql(newFolder.folder_name)
+          expect(res.body).to.have.property('id')
+          expect(res.headers.location).to.eql(`/api/folder/${res.body.id}`)
+        })
+        .then(res => {
+          supertest(app)
+            .get(`/api/folder/${res.body.id}`)
+            .expect(res.body)
+        })
+    })
+
+    const requiredFields = ['folder_name']
+
+    requiredFields.forEach(field => {
+      const newFolder = {
+        folder_name: 'Test Folder'
+      }
+
+      it(`Responds with 400 and an error when the ${field} is missing`, () => { // MISSING FIELD
+        delete newFolder[field]
+
+        return supertest(app)
+          .post('/api/folder')
+          .send(newFolder)
+          .expect(400, {
+            error: { message: `Missing ${key} in request body` }
+          })
+      })
+    }) 
+  })
+
+  describe('DELETE /api/folder/:folder_id') { // DELETE
+    context('Given no folders', () => { // GIVEN NO FOLDER
+      it('Responds with 404', () => { // RESPONDS 404
+        const folderId = 123456
+        return supertest(app)
+          .delete(`/api/folder/${folderId}`)
+          .expect(404, { error: { message: `Folder doesn't exist` } })
+      })
+    })
+
+    context('Given there are folders in the db', () => {
+      const testFolders = makeFolderArray()
+
+      beforeEach('insert folder', () => {
+        return db
+          .into('noteful_folder')
+          .insert(testFolders)
+          .then(() => {
+            return db
+              .into('noteful_folder')
+              .insert(testFolders)
+          })
+      })
+
+      it('Responds with 204 and then removes the folder', () => {
+        const idToRemove = 2
+        const expectedFolder = testFolders.filter(folder => folder.id !== idToRemove)
+        return supertest(app)
+          .delete(`/api/folder/${idToRemove}`)
+          .expect(204)
+          .then(res => {
+            supertest(app)
+              .get('/api/folder')
+              .expect(expectedFolder)
+          })
+      })
+    })
+  }
 })
